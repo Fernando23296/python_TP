49e2a3d1a9ceeb7efeeba45c11750fbbbf9f3a4a
Bruno <fernando23296@gmail.com>
2017-08-20 18:14:24 -0400

    Ejericio pagina 37

---
+++ exp51.py
@@ -0,0 +1,17 @@
+a=int(raw_input("intro nunm"))
+b=int(raw_input("intro nunm2"))
+
+if a<b:
+	print "a es menor q b"
+elif a>b:
+	print "a es mayor q b"
+else:
+	print "lo mismo"
+print "*"*20
+
+a=int(raw_input("xd"))
+if a>10:
+	pass
+else:
+	print "muy peque"
+print " "
